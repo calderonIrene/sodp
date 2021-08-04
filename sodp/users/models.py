3ed2b2fb0e53ecf61e58b1d2b1780325021a6a25
calderonIrene <80881547+calderonIrene@users.noreply.github.com>
2021-08-04 14:21:02 +0200

    Tresholds added on user model

--- sodp/users/models.py
+++ sodp/users/models.py
@@ -6,7 +6,6 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 from oauth2client import client
-
 import httplib2
 
 class User(AbstractUser):
